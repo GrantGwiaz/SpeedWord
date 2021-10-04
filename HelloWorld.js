73d79c49f88969996c5bcf38cf78e3794d29b737
Grant Gwiaz <tug66039@temple.edu>
2021-10-04 11:17:56 -0400

    Initial commit

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+// the hello world program
+function f1 () {
+    // print to console
+    console.log('Hello, World!');
+    //print to alert box
+    alert("Hello, World!");
+    //print to html document
+    document.open();
+    document.write("<h1>Hello World!</h1>");
+    document.close();
+}
